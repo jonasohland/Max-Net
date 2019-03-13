48f96e910bf9189b8d5b330e0b652df4934c178c
Jonas Ohland <jonas.ohland@gmail.com>
2019-03-13 20:14:17 +0100

    added license notices

--- source/shared/include/session_query_macros.h
+++ source/shared/include/session_query_macros.h
@@ -1,3 +1,28 @@
+//
+// This file is part of the Max Network Extensions Project
+//
+// Copyright (c) 2019, Jonas Ohland
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/variadic/to_seq.hpp>
@@ -14,19 +39,34 @@
     status != session_impl_type::status_codes::elem&&
 
 /* CON_WHERE_STATUS_OR(CON_IS(ONLINE), CON_IS(OFFLINE)) will expand to
- 
+ 
+
+
+
  [](std::vector<std::shared_ptr<websocket_connection>>::iterator _c) -> bool {
- 
+ 
+
+
+
     if(!*_c){ return false; }
- 
+ 
+
+
+
     auto status = (*_c)->status();
- 
+ 
+
+
+
     if(
         status == ohlano::websocket_connections::status_codes::ONLINE ||    <-- repeats
         status == ohlano::websocket_connections::status_codes::OFFLINE ||
         false
     ) { return true; } else { return false; }
- 
+ 
+
+
+
  }
  */
 #define CON_WHERE_STATUS_OR( ... )                                                       \
