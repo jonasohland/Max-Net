56f1436dabd81efb5637262248c1faeef42fa641
Jonas Ohland <jonas.ohland@gmail.com>
2019-03-26 22:26:05 +0100

    link to pthreads on linux

--- cmake/ohlib.cmake
+++ cmake/ohlib.cmake
@@ -3,6 +3,14 @@ macro(ohlib_setup input_target)
 set(Boost_USE_STATIC_LIBS ON)
 set(Boost_USE_MULTITHREADED ON)	
 
+find_package(Threads)
+
+
+if(CMAKE_USE_PTHREADS_INIT)
+	target_link_libraries(${input_target} PUBLIC ${CMAKE_THREAD_LIBS_INIT})
+endif()
+
+
 find_package(Boost COMPONENTS 
 					"system" 
 					"date_time" 
--- source/test.cpp
+++ source/test.cpp
@@ -5,7 +5,7 @@ int main() {
 
     boost::asio::io_context ctx;
 
-    o::io::every(ctx, std::chrono::seconds(3))
+    o::io::every(ctx, std::chrono::seconds(1))
         .repeat([counter = size_t()](boost::system::error_code ec) mutable {
             if (!ec) std::cout << "Counter is: " << counter << std::endl;
             return ++counter >= 10 || ec;
