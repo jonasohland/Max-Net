efde30a055a2cc30bc17ac4b71ce30a9e04de6ce
Jonas Ohland <jonas.ohland@gmail.com>
2019-03-27 15:48:12 +0100

    documentation

--- source/shared/example/timer.cpp
+++ source/shared/example/timer.cpp
@@ -9,27 +9,52 @@ using app_base = o::io::signal_listener_app<o::ccy::none>;
 
 struct app : public app_base {
 
+    // the app was started via the run() call
     virtual void on_app_started() override {
 
+        // create a new timer that waits 5 seconds
         timer_ =
             o::io::wait(this->context(), 5s).then([this](sys::error_code ec) {
-
+                // do nothing if ec holds a value (an error ocurred, or the
+                // operation was cancelled)
                 if (ec) return;
 
                 std::cout << "Timer Callback" << std::endl;
 
+                // allow the application to exit
                 this->app_allow_exit();
-                this->signals_stop();
+            });
 
+        // Annoy the user every 500 milliseconds
+        o::io::every(this->context(), 500ms)
+            .repeat([this](boost::system::error_code ec) {
+                // Annoy
+                std::cout << "Hello!" << std::endl;
+
+                // Return true (stop repeating) if the stop flag was set or ec
+                // holds a value (an error ocurred, or the operation was
+                // cancelled)
+                return ec || stop;
             });
     }
 
+    // The app was allowed to exit either through a signal from the os or via
+    // the app_allow_exit() call
     virtual void on_app_exit(int reason) override {
 
         std::cout << "Exit: " << reason << std::endl;
 
+        // stop waiting for signals
+        this->signals_stop();
+
+        // set the stop flag
+        stop = true;
+
+        // cancel the exit timeout
         if (timer_) timer_->cancel();
     }
+
+    bool stop = false;
 
     std::shared_ptr<boost::asio::steady_timer> timer_;
 };
--- source/shared/include/o/io/timer.h
+++ source/shared/include/o/io/timer.h
@@ -1,24 +1,57 @@
 #pragma once
 
+/**
+ * @file
+ *
+ * Declares some useful timer extensions
+ */
+
+#include "../types.h"
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/steady_timer.hpp>
 #include <type_traits>
-#include "../types.h"
 
 namespace o::io {
 
     namespace detail {
 
+        /**
+         * A waiting timer.
+         *
+         * @author  Jonas Ohland
+         * @date    27.03.2019
+         *
+         * @tparam  Duration    Type of the duration.
+         * @tparam  Clock       Type of the clock.
+         */
         template <typename Duration, typename Clock>
         class waiting_timer {
-            
-            using waitable_timer_type = boost::asio::basic_waitable_timer<Clock>;
+
+            /** Type of the waitable timer */
+            using waitable_timer_type =
+                boost::asio::basic_waitable_timer<Clock>;
 
           public:
+            /**
+             * Constructor
+             *
+             * @author  Jonas Ohland
+             * @date    27.03.2019
+             *
+             * @param [in,out]  ctx The context.
+             * @param           d   A Duration to process.
+             */
             waiting_timer(boost::asio::io_context& ctx, Duration d)
-                : wait_time_(d)
-                , temp_timer_(new waitable_timer_type(ctx)) {}
+                : wait_time_(d), temp_timer_(new waitable_timer_type(ctx)) {}
 
+            /**
+             * Thens the given handler
+             *
+             * @tparam  Handler Type of the handler.
+             * @param [in,out]  handler The handler.
+             *
+             * @returns A std::shared_ptr&lt;waitable_timer_type&gt;
+             */
             template <typename Handler>
             std::shared_ptr<waitable_timer_type> then(Handler&& handler) {
 
@@ -29,47 +62,98 @@ namespace o::io {
                               "wait handler requirements not met");
 
                 auto output = temp_timer_;
-                
+
                 temp_timer_->expires_after(wait_time_);
 
-                temp_timer_->async_wait([
-                    handler_l = handler, capt_timer = std::move(temp_timer_)
-                ](boost::system::error_code ec) { handler_l(ec); });
+                temp_timer_->async_wait(
+                    [handler_l = handler, capt_timer = std::move(temp_timer_)](
+                        boost::system::error_code ec) { handler_l(ec); });
 
                 return output;
             }
 
           private:
+            /** The wait time */
             Duration wait_time_;
+            /** The temporary timer */
             std::shared_ptr<waitable_timer_type> temp_timer_;
         };
 
+        /**
+         * A temporary timer object, that will schedule a
+         * boost::asio::basic_waitable_timer&lt;Clock&gt; when a callback is
+         * assigned.
+         *
+         * @author  Jonas Ohland
+         * @date    27.03.2019
+         *
+         * @tparam  Duration    Type of the duration.
+         * @tparam  Clock       Type of the clock.
+         */
         template <typename Duration, typename Clock>
         class repeating_timer {
 
           public:
-        
-            using waitable_timer_type = boost::asio::basic_waitable_timer<Clock>;
-            
+            /** Type of the waitable timer */
+            using waitable_timer_type =
+                boost::asio::basic_waitable_timer<Clock>;
+
+            /**
+             * A functor object that will reschedule itself on a timer after a
+             * given duration is over. This class can be used to invoke a
+             * callback repeatedly from the timer.
+             *
+             * @author  Jonas Ohland
+             * @date    27.03.2019
+             *
+             * @tparam  Handler Handler type. Might be a result from
+             * `std::bind`, a lambda or a functor. Must have the signature:
+             *                  `bool(boost::system::error_code)`.
+             */
             template <typename Handler>
             struct recursor {
 
-                recursor(Handler&& input_handler, Duration input_duration,
+                /**
+                 * Constructor
+                 *
+                 * @author  Jonas Ohland
+                 * @date    27.03.2019
+                 *
+                 * @param [in]  input_handler   The input handler.
+                 * @param       input_duration  Duration of the input.
+                 * @param [in]  timer           The timer.
+                 */
+                recursor(Handler&& input_handler, Duration&& input_duration,
                          std::shared_ptr<waitable_timer_type>&& timer)
                     : handler(std::forward<Handler>(input_handler))
-                    , duration(input_duration)
-                    , tm_(timer) {}
+                    , duration(std::forward<Duration>(input_duration))
+                    , tm_(std::forward<std::shared_ptr<waitable_timer_type>>(
+                          timer)) {}
 
-                recursor(const recursor& other)
-                    : handler(other.handler)
-                    , duration(other.duration)
-                    , tm_(other.tm_) {}
+                /**
+                 * Constructor
+                 *
+                 * @author  Jonas Ohland
+                 * @date    27.03.2019
+                 *
+                 * @param   other   The other.
+                 */
+                recursor(const recursor& other) = default;
 
+                /**
+                 * Function call operator
+                 *
+                 * @author  Jonas Ohland
+                 * @date    27.03.2019
+                 *
+                 * @param   ec  The ec.
+                 */
                 void operator()(boost::system::error_code ec) {
-                    
-                    // If you hit a static_assert failure here, you propably forgot
-                    // to return a value from the wait handler for .repeat(Handler)
-                    // In most cases it should be suitable to simply return the error_code
+
+                    // If you hit a static_assert failure here, you propably
+                    // forgot to return a value from the wait handler for
+                    // .repeat(Handler) In most cases it should be suitable to
+                    // simply return the error_code
 
                     static_assert(
                         std::is_convertible<decltype(handler(
@@ -83,24 +167,47 @@ namespace o::io {
 
                     if (!handler(ec)) {
                         tm_->expires_after(duration);
-                        tm_->async_wait(recursor<Handler>(
-                            std::move(handler), duration, std::move(tm_)));
+                        tm_->async_wait(recursor<Handler>(std::move(handler),
+                                                          std::move(duration),
+                                                          std::move(tm_)));
                     }
                 }
 
                 Handler handler;
+
                 Duration duration;
+                /** the timer to wait on */
                 std::shared_ptr<waitable_timer_type> tm_;
             };
 
+            /**
+             * Constructor
+             *
+             * @author  Jonas Ohland
+             * @date    27.03.2019
+             *
+             * @param [in,out]  ctx The context.
+             * @param           d   A Duration to process.
+             */
             repeating_timer(boost::asio::io_context& ctx, Duration d)
-                : temp_timer_(new waitable_timer_type(ctx))
-                , next_tick_(d) {
-            }
+                : temp_timer_(new waitable_timer_type(ctx)), next_tick_(d) {}
 
+            /**
+             * Start repeating the given Handler. The handler must have the
+             * following signature: \code{.cpp} bool
+             * func(boost::system::error_code ec){
+             *     return my_continue_condition  || ec;
+             * }
+             * \endcode
+             *
+             * @tparam  Handler Type of the handler.
+             * @param [in]  handler The handler.
+             *
+             * @returns A `std::shared_ptr&lt;waitable_timer_type&gt;` from
+             * which the Handler will be called.
+             */
             template <typename Handler>
-            std::shared_ptr<waitable_timer_type>
-            repeat(Handler&& handler) {
+            std::shared_ptr<waitable_timer_type> repeat(Handler&& handler) {
 
                 // the repeat handler must have the following signature:
                 // bool(boost::system::error_code) or
@@ -110,31 +217,58 @@ namespace o::io {
                               "repeat handler requirements not met");
 
                 auto output = temp_timer_;
-                
+
                 temp_timer_->expires_after(next_tick_);
 
-                temp_timer_->async_wait(
-                    recursor<Handler>(std::forward<Handler>(handler),
-                                      next_tick_, std::move(temp_timer_)));
+                temp_timer_->async_wait(recursor<Handler>(
+                    std::forward<Handler>(handler), std::move(next_tick_),
+                    std::move(temp_timer_)));
 
                 return output;
             }
 
           private:
+            /** The temporary timer */
             std::shared_ptr<waitable_timer_type> temp_timer_;
+            /** The next tick */
             Duration next_tick_;
         };
-    }
+    } // namespace detail
 
+    /**
+     * Create a new temporary waiting timer object, that may be used to execute
+     * a callback at later time. The waiting period will begin when the callback
+     * is assigned to the timer.
+     *
+     * @tparam  Duration    Type of the duration. Example:
+     *                      std::chrono::milliseconds.
+     * @tparam  Clock       Type of the clock.
+     * @param [in]  ctx The asio::io_context to wait on.
+     * @param           d   The duration to wait between the repeated calls.
+     *
+     * @returns A temporary waiting_timer object.
+     */
     template <typename Duration, typename Clock = std::chrono::steady_clock>
     detail::waiting_timer<Duration, Clock> wait(boost::asio::io_context& ctx,
-                                         Duration d) {
+                                                Duration d) {
         return detail::waiting_timer<Duration, Clock>(ctx, d);
     }
 
+    /**
+     * Create a new temporary waiting timer object, that may be used to execute
+     * a callback repeatedly. Repeated execution will start when the callback is
+     * assigned. is assigned to the timer.
+     *
+     * @tparam  Duration    Type of the duration.
+     * @tparam  Clock       Type of the clock.
+     * @param [in]  ctx The `boost::asio::io_context` to wait on.
+     * @param           d   The duration to wait.
+     *
+     * @returns A temporary repeating_timer object.
+     */
     template <typename Duration, typename Clock = std::chrono::steady_clock>
     detail::repeating_timer<Duration, Clock> every(boost::asio::io_context& ctx,
-                                            Duration d) {
+                                                   Duration d) {
         return detail::repeating_timer<Duration, Clock>(ctx, d);
     }
-}
+} // namespace o::io
